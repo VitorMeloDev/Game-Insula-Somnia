39d311cfeed93a1a1b2e77460e63e28a204ae59c
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2024-07-12 09:29:09 -0300

    Game Manager Added

--- Insula Somnia/Assets/Scripts/SlimeIA.cs
+++ Insula Somnia/Assets/Scripts/SlimeIA.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -9,7 +10,7 @@ public class SlimeIA : MonoBehaviour
     private Animator anim;
     private bool isDie = false;
     public int HP = 3;
-    
+    public enemyState state;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,7 @@ public class SlimeIA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        StateManager();
     }
 
     IEnumerator Die()
@@ -44,4 +45,64 @@ public class SlimeIA : MonoBehaviour
             StartCoroutine(Die());
         }
     }
+
+    void StateManager()
+    {
+        switch (state)
+        {
+            case enemyState.IDLE:
+
+            break;
+
+            case enemyState.ALERT:
+            
+            break;
+            
+            case enemyState.EXPLORE:
+
+            break;
+
+            case enemyState.FOLLOW:
+
+            break;
+
+            case enemyState.FURY:
+
+            break;
+            
+            case enemyState.PATROL:
+
+            break;
+        }
+    }
+
+    void ChangeState(enemyState newState)
+    {
+        switch (state)
+        {
+            case enemyState.IDLE:
+
+            break;
+
+            case enemyState.ALERT:
+            
+            break;
+            
+            case enemyState.EXPLORE:
+
+            break;
+
+            case enemyState.FOLLOW:
+
+            break;
+
+            case enemyState.FURY:
+
+            break;
+            
+            case enemyState.PATROL:
+
+            break;
+        }
+    }  
 }
